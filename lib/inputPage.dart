e9930f343c0932c9ce42a7c2ceea1020048d7025
Tarlochan5268 <brotherstarlochan5268@gmail.com>
2020-06-28 02:19:54 -0400

    Refractored the container widget to make new Reusable Card

--- lib/inputPage.dart
+++ lib/inputPage.dart
@@ -12,9 +12,49 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
-      body: Center(
-        child: Text('Body Text'),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: ReusableCard(),
+                ),
+                Expanded(
+                  child: ReusableCard(),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ReusableCard(),
+          ),
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: ReusableCard(),
+                ),
+                Expanded(
+                  child: ReusableCard(),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
+
+      /*
+        body: Container(
+        height: 200,
+        width: 170,
+        margin: EdgeInsets.all(15),
+        decoration: BoxDecoration(
+          color: Color(0xFF1D1E33), //color property go inside decoration
+          borderRadius: BorderRadius.circular(10),
+        ),
+      ),
+      */
       //just wrap any widget inside Theme widget to change its theme
       /*
         floatingActionButton: Theme(
@@ -24,6 +64,26 @@ class _InputPageState extends State<InputPage> {
         ),
       ),
       */
+    );
+  }
+}
+
+//Refactoring of widget
+class ReusableCard extends StatelessWidget {
+  const ReusableCard({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 200,
+      width: 170,
+      margin: EdgeInsets.all(15),
+      decoration: BoxDecoration(
+        color: Color(0xFF1D1E33), //color property go inside decoration
+        borderRadius: BorderRadius.circular(10),
+      ),
     );
   }
 }
